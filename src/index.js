e68c78f6998cf578f5a1cebb82a5cabdb7c5ee18
Dominique Skaf <doskaf@yahoo.com>
2020-02-21 18:46:42 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
   // your code here
 });
 let submitButton = document.getElementsByTagName("input")[1];
-document.getElementsByTagName("input")[1].addEventListener("click", function(event) {
+submitButton.addEventListener("click", function(event) {
          document.getElementById("output-box").innerHTML += "Sorry! <code>preventDefault()</code> won't let you check this!<br>";
          event.preventDefault();
 }, false);
