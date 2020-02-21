dbc9d18ab96f39626e848cf246bef5fb8ec8fcb4
Dominique Skaf <doskaf@yahoo.com>
2020-02-21 19:11:00 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ let submitButton = document.getElementsByTagName("input")[1];
 
 submitButton.addEventListener("click", function(event) {
   let node = document.createElement("LI");
-  let textnode = document.createTextNode(newTaskBox.value);
+  let textnode = document.createTextNode(`${newTaskBox.value} `);
   node.appendChild(textnode);
   document.getElementById('tasks').appendChild(node);
   let button = document.createElement("BUTTON");
