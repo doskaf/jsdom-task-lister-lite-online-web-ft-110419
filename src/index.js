fb16b4bf4700b83f1511de011cfb158c01becc10
Dominique Skaf <doskaf@yahoo.com>
2020-02-21 18:55:44 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ let submitButton = document.getElementsByTagName("input")[1];
 
 submitButton.addEventListener("click", function(event) {
   let node = document.createElement("LI");
-  let textnode = document.createTextNode();
+  let textnode = document.createTextNode("Clicked!");
   node.appendChild(textnode);
   document.getElementById('tasks').appendChild(node);
   event.preventDefault();
