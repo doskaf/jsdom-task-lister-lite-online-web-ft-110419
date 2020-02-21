27162884e8af2886d1fde9ae331713a806c6d731
Dominique Skaf <doskaf@yahoo.com>
2020-02-21 18:56:13 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ document.addEventListener("DOMContentLoaded", () => {
   // your code here
 });
 
+
+
 let submitButton = document.getElementsByTagName("input")[1];
 
 submitButton.addEventListener("click", function(event) {
