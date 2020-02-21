69b7ce34b4457ff228b3021731dbdb486419f591
Dominique Skaf <doskaf@yahoo.com>
2020-02-21 18:52:29 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -4,6 +4,6 @@ document.addEventListener("DOMContentLoaded", () => {
 let submitButton = document.getElementsByTagName("input")[1];
 
 submitButton.addEventListener("click", function(event) {
-  ;
+  document.getElementById('tasks').appendChild;
   event.preventDefault();
 }, false);
