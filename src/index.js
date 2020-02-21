f76096dcad0092ea92e2b0db925bd1ca73afa204
Dominique Skaf <doskaf@yahoo.com>
2020-02-21 18:54:40 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -5,7 +5,8 @@ let submitButton = document.getElementsByTagName("input")[1];
 
 submitButton.addEventListener("click", function(event) {
   let node = document.createElement("LI");
-  let textnode = document.createTextNode("Water");   
+  let textnode = document.createTextNode();
+  
   document.getElementById('tasks').appendChild(node);
   event.preventDefault();
 }, false);
