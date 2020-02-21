36f58861e5d1a130b675f95dd6c0ff656ff2b34b
Dominique Skaf <doskaf@yahoo.com>
2020-02-21 18:46:15 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
   // your code here
 });
 
-document.querySelector("#id-checkbox").addEventListener("click", function(event) {
+document.getElementsByTagName("input")[1].addEventListener("click", function(event) {
          document.getElementById("output-box").innerHTML += "Sorry! <code>preventDefault()</code> won't let you check this!<br>";
          event.preventDefault();
 }, false);
